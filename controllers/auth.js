37e809cb7d9da8884568910e6920ca1e66ca9fa6
Hyper-terminal <abhisharma001001001@gmail.com>
2022-08-15 16:02:57 +0530

    editted signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,7 +39,7 @@ exports.signin = (req, res) => {
         // if err or no user
         if (err || !user) {
             return res.status(401).json({
-                error: "User with that email doesn't exist. Please signin."
+                error: "User with that email doesn't exist. Please signup."
             })
         }
         // if user is found make sure password and email match
